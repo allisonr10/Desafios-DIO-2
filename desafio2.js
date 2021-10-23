b2b45ec21ddc245a62d637f96abe016472041be1
Allison Rodrigues <allison.ifg@gmail.com>
2021-10-23 10:58:46 -0300

    Corrigindo erro
    Erro de dois números no mesmo intervalo corrigido.

--- desafio2.js
+++ desafio2.js
@@ -9,13 +9,13 @@ function intervalo(){
     if(numeroArredondado >= 0 && numeroArredondado <= 25){
         console.log('Intervalo de [0,25]')
     }
-    else if(numeroArredondado >= 25 && numeroArredondado <= 50){
+    else if(numeroArredondado > 25 && numeroArredondado <= 50){
         console.log('Intervalo de [25,50]')
     }
-    else if (numeroArredondado >= 50 && numeroArredondado <= 75){
+    else if (numeroArredondado > 50 && numeroArredondado <= 75){
         console.log('Intervalo de [50,75]')
     }
-    else if (numeroArredondado >= 75 && numeroArredondado <= 100){
+    else if (numeroArredondado > 75 && numeroArredondado <= 100){
         console.log('Intervalo de [75,100]')
     }
     else{
